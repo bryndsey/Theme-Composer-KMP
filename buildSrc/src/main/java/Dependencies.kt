4e938a140165f75959e9b20d2e662c160a8c697c
Bryan Lindsey <blinz117@gmail.com>
2021-04-11 07:40:29 -0500

    Update to latest compose version

--- buildSrc/src/main/java/Dependencies.kt
+++ buildSrc/src/main/java/Dependencies.kt
@@ -1,7 +1,7 @@
  object Versions {
-     val kotlinVersion = "1.4.31"
+     val kotlinVersion = "1.4.32"
 
-      val composeVersion = "0.4.0-build177"
+      val composeVersion = "0.4.0-build180"
 
      val coroutinesVersion = "1.4.2"
      val hiltVersion = "2.33-beta"
